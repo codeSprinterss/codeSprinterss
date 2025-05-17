379a5456081b96a238e74573f5a4ed399826d3de
chungchae <chungchae0@gmail.com>
2025-05-17 18:12:23 +0900

    PGS/L2/큰수만들기

---
+++ chungchae/greedy/PGS_큰수만들기.cpp
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+
+using namespace std;
+
+// PG 큰 수 만들기 (greedy) (L2)
+
+string solution(string number, int k) {
+    string answer = "";
+
+    // number 배열의 각 숫자에 대해서
+    for (char digit : number) {
+        while (!answer.empty() && answer.back() < digit && k > 0) {
+            answer.pop_back();
+            k--;
+        }
+        answer.push_back(digit);
+    }
+
+    if (k > 0) answer = answer.substr(0, answer.size() - k);
+
+    return answer;
+}
