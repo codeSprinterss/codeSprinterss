0b64e0b878a9f4c8c5d7fdd5ea1266219b407ab2
chungchae <chungchae0@gmail.com>
2025-05-22 20:51:46 +0900

    BOJ/S1/정수삼각형

---
+++ chungchae/DP/BOJ_1932.cpp
@@ -0,0 +1,42 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+
+using namespace std;
+
+// BOJ 1932 정수 삼각형 (DP) (S1)
+
+using namespace std;
+
+int main() {
+	int N; cin >> N;
+	vector<vector<int>> arr(N);
+	int temp;
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < i + 1; j++) {
+			cin >> temp;
+			arr[i].push_back(temp);
+		}
+	}
+
+	//아래 두 값 중, 큰 값을 더하기를 반복
+	for (int i = N - 1; i > 0; i--) {
+		for (int j = 0; j < i; j++) {
+			arr[i - 1][j] += max(arr[i][j], arr[i][j + 1]);
+		}
+	}
+	cout << arr[0][0];
+}
+
