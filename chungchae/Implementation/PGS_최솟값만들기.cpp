24859f41e2ecad47a2f7874c440798110f17ab0e
chungchae <chungchae0@gmail.com>
2025-05-25 22:15:16 +0900

    PGS/L2/최솟값만들기

---
+++ chungchae/Implementation/PGS_최솟값만들기.cpp
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+
+using namespace std;
+
+// PGS 최솟값 만들기 (Implementation) (L2)
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B)
+{
+    int answer = 0;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end(), [](const int& a, const int& b) {
+        return a > b;
+        });
+
+    for (int i = 0; i < A.size(); i++) {
+        answer += A[i] * B[i];
+    }
+
+    return answer;
+}
+
