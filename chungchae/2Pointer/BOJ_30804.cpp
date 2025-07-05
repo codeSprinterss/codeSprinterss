58a9fa21bd3c45e5827ad6cc26d1bf537dc56190
chungchae <chungchae0@gmail.com>
2025-07-05 23:55:39 +0900

    BOJ/S2/과일탕후루

---
+++ chungchae/2Pointer/BOJ_30804.cpp
@@ -0,0 +1,56 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+
+using namespace std;
+
+// BOJ 30804 과일 탕후루 (2-pointer) (S2)
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N; cin >> N;
+
+	vector<int> arr(N, 0);
+	set<int> S;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+	int a = 0; int b = 0;
+	unordered_map<int, int> M;
+
+	int maxLen = 0;
+
+	while (b < N) {
+		M[arr[b]]++;
+
+		while (M.size() > 2) {
+			M[arr[a]]--;
+			if (M[arr[a]] == 0) {
+				M.erase(arr[a]);
+			}
+			a++;
+		}
+
+		maxLen = max(maxLen, b - a + 1);
+		b++;
+	}
+
+	cout << maxLen;
+}
+
+
