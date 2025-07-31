1c76dea1c154bb4506826cba31ba5f01034143f4
chungchae <chungchae0@gmail.com>
2025-07-31 23:32:21 +0900

    BOJ/S5/줄세우기

---
+++ chungchae/Implementation/BOJ_10431.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <limits.h>
+
+using namespace std;
+
+// BOJ 10431 줄세우기 (Implementation) (S5)
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N; cin >> N;
+
+	for (int total = 0; total < N; total++) {
+		int now; cin >> now;
+		vector<int> arr(20);
+
+		for (int i = 0; i < 20; i++) {
+			cin >> arr[i];
+		}
+		int count = 0;
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < i; j++) {
+				if (arr[j] > arr[i])
+					count++;
+			}
+		}
+
+		cout << total + 1 << " " << count << "\n";
+	}
+}
