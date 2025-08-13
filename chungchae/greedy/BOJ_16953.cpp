e07211c56d5fd99d1bb6eef863ebbe1f2e29526f
chungchae <chungchae0@gmail.com>
2025-08-13 14:57:13 +0900

    BOJ/S2/A->B

---
+++ chungchae/greedy/BOJ_16953.cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <limits.h>
+
+using namespace std;
+
+// BOJ 16953 A->B (greedy) (S2)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A, B; cin >> A >> B;
+    int count = 0;
+
+    while (A < B) {
+        if (B % 2 == 0) {
+            B /= 2;
+        }
+        else {
+            // 81 -> 8
+            if (B % 10 == 1) {
+                B /= 10;
+            }
+            else break;
+        }
+        count++;
+    }
+    if (A == B)
+        cout << count + 1;
+    else
+        cout << -1;
+}
