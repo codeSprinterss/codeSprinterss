e6819c400bf0cae0144c9df1f586df52e5071d9f
chungchae <chungchae0@gmail.com>
2025-08-18 22:53:23 +0900

    BOJ/S5/모바일광고입찰

---
+++ chungchae/sort/BOJ_31246.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <numeric>
+#include <stack>
+#include <queue>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <limits.h>
+
+using namespace std;
+
+// BOJ 31246 모바일 광고 입찰 (sort) (S5)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, K; cin >> N >> K;
+
+    vector<int> arr;
+    for (int i = 0; i < N; i++) {
+        int a, b; cin >> a >> b;
+
+        arr.push_back(a - b);
+    }
+
+    sort(arr.begin(), arr.end(), greater<int>());
+
+    if (arr[K - 1] >= 0) {
+        cout << 0;
+        return 0;
+    }
+    else {
+        cout << -arr[K - 1];
+        return 0;
+    }
+}
